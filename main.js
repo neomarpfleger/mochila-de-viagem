0141538fbd9e8633ffc2eb8cc9d547018890a58f
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-01-31 21:00:26 -0300

    30012023

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const form=document.getElementById("novoItem")
+
+form.addEventListener("submit", (evento)=>{
+    evento.preventDefault()
+    console.log(evento)
+    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value)
+})
+
+function criaElemento(nome, quantidade){
+console.log(nome)
+console.log(quantidade)
+}
