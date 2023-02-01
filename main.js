18ea5d1b4842ef182ea17a05f24d80a741d80cc8
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-02-01 19:45:23 -0300

    31012023

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 
-
-const form=document.getElementById("novoItem")
+const form=document.getElementById("novoItem");
 
 form.addEventListener("submit", (evento)=>{
-    evento.preventDefault()
-    console.log(evento)
-    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value)
+    evento.preventDefault();
+    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value);
 })
 
 function criaElemento(nome, quantidade){
-console.log(nome)
-console.log(quantidade)
+
+    const novoItem =document.createElement("li");
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement("strong");
+    numeroItem.innerHTML = quantidade;
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+    
+    const lista = document.getElementById("lista")
+    lista.appendChild(novoItem)
 }
