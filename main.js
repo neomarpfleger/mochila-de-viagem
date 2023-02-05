2e64018906da94dd63763e70f76e77522cf49012
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-02-05 07:29:13 -0300

    03022023

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = []
 
-const form=document.getElementById("novoItem");
+form.addEventListener("submit", (evento)=>{
+    evento.preventDefault();
+
+    const nome = (evento.target.elements["nome"].value)
+    const quantidade = (evento.target.elements["quantidade"].value)
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = ""
+    quantidade.value= ""
+})
+
+function criaElemento(nome, quantidade){
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem =document.createElement("strong");
+    numeroItem.innerHTML= quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem);
+
+    const atual{
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringity(itens))
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const form=document.getElementById("novoItem");
 
 form.addEventListener("submit", (evento)=>{
     evento.preventDefault();
@@ -18,4 +77,4 @@ function criaElemento(nome, quantidade){
     
     const lista = document.getElementById("lista")
     lista.appendChild(novoItem)
-}
+}*/
