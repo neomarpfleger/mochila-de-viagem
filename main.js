c48be6ff1e8f5d0c39a8ca3c859a8899fb0b8b88
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-02-06 19:24:06 -0300

    06022023

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-const form = document.getElementById("novoItem");
+const form = document.getElementById("novoItem")
+
+form,addEventListener("submit", (evento)=>{
+    evento.preventDefault()
+    
+    const nome= (evento.target.elements["nome"].value)
+    const quantidade= (evento.target.elements["quantidade"].value)
+
+    criaElemento(nome.value, quantidade.value)
+})
+
+/*<li class="item"><strong>7</strong>Camisas</li>*/
+
+function criaElemento (nome, quantidade){
+
+    const novoItem = document.createElement("li")    
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML= quantidade
+
+    console.log(novoItem)
+    console.log(numeroItem)
+
+
+}
+
+
+
+
+
+/*const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = []
 
@@ -34,22 +65,7 @@ function criaElemento(nome, quantidade){
     itens.push(itemAtual);
 
     localStorage.setItem("item", JSON.stringity(itens))
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
