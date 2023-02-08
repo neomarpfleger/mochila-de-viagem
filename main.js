7f11747244a22b2515a12be528167990df244b03
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-02-08 19:27:09 -0300

    08022023
    Problema resolvido ao atribuir um valor a uma constante não utilizar ( ).

--- main.js
+++ main.js
@@ -1,44 +1,4 @@
-/*const form = document.getElementById("novoItem") 
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-itens.forEach( (elemento) => {    
-    criaElemento(elemento)
-} )
-
-form.addEventListener("submit", (evento) => {  
-    evento.preventDefault()
-
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
-
-    const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
-    }
-
-    criaElemento(itemAtual)
-
-    itens.push(itemAtual)
-
-    localStorage.setItem("itens", JSON.stringify(itens))
-
-    nome.value = ""
-    quantidade.value = ""
-})
-
-function criaElemento(item) {  
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
-
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = item.quantidade
-    novoItem.appendChild(numeroItem)
-
-    novoItem.innerHTML += item.nome
-
-    lista.appendChild(novoItem)*/
-}
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || []
@@ -50,8 +10,8 @@ itens.forEach((elemento) => {
 form.addEventListener("submit", (evento)=>{
     evento.preventDefault();
 
-    const nome = (evento.target.elements["nome"])
-    const quantidade = (evento.target.elements["quantidade"])
+    const nome = evento.target.elements["nome"]
+    const quantidade = evento.target.elements["quantidade"]
 
     const itemAtual ={
         "nome": nome.value,
