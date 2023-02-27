75685ec60da7e62af52a3462be21355cd7a75096
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-02-27 20:43:46 -0300

    26022023

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-/*const form = document.getElementById("novoItem");
+const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
@@ -41,27 +41,4 @@ function criaElemento(item){
 
     lista.appendChild(novoItem);
 
-}*/
-
-
-const form=document.getElementById("novoItem");
-
-form.addEventListener("submit", (evento)=>{
-    evento.preventDefault();
-    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value);
-    console.log(nome)
-})
-
-function criaElemento(nome, quantidade){
-
-    const novoItem =document.createElement("li");
-    novoItem.classList.add("item");
-
-    const numeroItem = document.createElement("strong");
-    numeroItem.innerHTML = quantidade;
-    novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
-    
-    const lista = document.getElementById("lista")
-    lista.appendChild(novoItem)
 }
