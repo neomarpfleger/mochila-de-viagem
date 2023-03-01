a3a16b7710cc6299061c1d90440dcb657f7e4ecb
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-02-28 21:41:36 -0300

    28022023
    Feito a funcionalidade do botão excluir.

--- main.js
+++ main.js
@@ -47,13 +47,37 @@ function criaElemento(item){
 
     const numeroItem = document.createElement("strong");
     numeroItem.innerHTML= item.quantidade;
+
     numeroItem.dataset.id = item.id;
+
     novoItem.appendChild(numeroItem);
+
     novoItem.innerHTML += item.nome;
+
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item){
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao =document.createElement("button");
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function(){
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id){
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+    
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
